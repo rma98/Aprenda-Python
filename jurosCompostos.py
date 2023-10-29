34bea12f4ee5d146995cf1272e3b90884f3d5d37
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-29 16:25:09 -0300

    jurosCompostos.py UPDATE #2

--- jurosCompostos.py
+++ jurosCompostos.py
@@ -3,17 +3,27 @@ nome = input('Nome: ')
 print(f'Olá {nome}, seja muito bem-vindo aos juros compostos! Aqui nos cuidamos da sua aposentadoria!')
 
 dinheiro_inicial = int(input('Digite a quantidade de dinheiro inicial do investimento: '))
-tempo = int(input('Digite quantos anos você deixará o dinheiro invetido: '))
-taxa = float(input('Digite, em decimal, qual a taxa anual de retorno do seu investimento: '))
+tempo_desejado = int(input('Digite quantos meses você deixará o dinheiro invetido: '))
+taxa = float(input('Digite, em decimal, qual a taxa mensal de retorno do seu investimento: '))
+aporte = float(input('Quanto você consegue aportar por mês: '))
 salario_desejado = int(input('Digite o salário mensal que você quer receber ao se aposentar: '))
 
-montante = dinheiro_inicial * (1 + taxa) ** tempo
-montante = round(montante, 2)
+tempo_decorrido = 0
+
+while tempo_decorrido < tempo_desejado:
+    if tempo_decorrido == 0:
+        montante = dinheiro_inicial * (1 + taxa) ** 1
+        montante = round(montante, 2)
+        tempo_decorrido += 1
+    else:
+        montante = (montante + aporte) * (1 + taxa) ** 1
+        montante = round(montante, 2)
+        tempo_decorrido += 1
 
 print(f'{"-"*70}')
 print(f'Parabéns {nome}! Ao final do período você terá R${montante}!')
 
-salario_mensal = (montante * taxa) / 12
+salario_mensal = (montante * taxa)
 salario_mensal = round(salario_mensal, 2)
 
 if salario_mensal >= salario_desejado:
